b9a0ce5b70197fb2bd092bf02dbc3199c696794b
Gratian Dicu <gratian.dicu@softvision.com>
2025-06-12 10:01:28 +0300

    Add command-line options for log processing and version display

--- index.js
+++ index.js
@@ -13,6 +13,48 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { useServer } from 'graphql-ws/use/ws';
 import cors from 'cors';
 
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Read and parse package.json
+const packageJson = JSON.parse(
+  readFileSync(join(__dirname, 'package.json'), 'utf-8')
+);
+const version = packageJson.version;
+
+const args = process.argv;
+
+if (args.length>2) {
+  const option = args[2];
+  switch (option) {
+    case '-h': 
+      console.log(`Ohana CleanLogs:
+  This script starts an Apollo subscription on localhost:4000/graphql that will replicate the 
+  logs line by line from a logsession.txt file.
+  This file should be a "dump" log file from npm run start | tee -a logsession.txt
+        Also it cleans and parses the logs.
+        -v: Show the version of the script
+        -u: Show an usage example`);
+      break;
+    case '-v':
+      console.log(`Version: ${version}`)
+      break;
+    case '-u':
+      console.log(`Usage:
+        > tail -f logsession.txt | cleanlogs`);
+      break;
+    default:
+      console.log("Try running > cleanlogs -h");
+      break;
+  }
+  process.exit(1);
+}
+
 
 const typeDefs = `
   type Message {
